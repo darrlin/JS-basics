bde73db63815604b343aeb9e805a965dd7422d86
Daria <daria.matskov19@gmail.com>
2021-08-06 14:02:25 +0300

    Js + react 1

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// const result = confirm('Гей?');
+// console.log(result);
+
+// const answer = +prompt('Гей?', '17'); 
+// console.log(answer + 1);
+
+// const answers = [];
+
+// answers[0] = prompt('What your name?', '');
+// answers[1] = prompt('What your lastname?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// document.write(answers);
+
+//`Интерполяция${}`
+
+//console.log(4 + +'5');
