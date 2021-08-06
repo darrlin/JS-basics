8309f6b086f562a9287a70cc79d6821dd2516f2e
Daria <daria.matskov19@gmail.com>
2021-08-06 19:55:53 +0300

    message

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// const result = confirm('Гей?');
+// const result = confirm('Gay?');
 // console.log(result);
 
-// const answer = +prompt('Гей?', '17'); 
+// const answer = +prompt('Age', '17'); 
 // console.log(answer + 1);
 
 // const answers = [];
@@ -17,3 +17,26 @@
 //`Интерполяция${}`
 
 //console.log(4 + +'5');
+
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+const pusObj = {},
+      pusObj2 = {};
+const arr = [];
+
+const personalMovirDB = {
+    count: numberOfFilms,
+    movies: pusObj,
+    actors: pusObj2,
+    genres: arr,
+    privat: false
+};
+
+const a = prompt('Один из последних просомтренных вами фильмов?', ''),
+      b = +prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просомтренных вами фильмов?', ''),
+      d = +prompt('На сколько оцените его?', '');
+
+personalMovirDB.movies[a] = b;
+personalMovirDB.movies[c] = d;
+
+console.log(personalMovirDB);
